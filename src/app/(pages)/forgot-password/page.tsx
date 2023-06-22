dc39eed5afb834660840c7f15fedef0596407e8d
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2023-06-23 00:22:13 +0100

    feat: add success feedback to forgot-password page

--- src/app/(pages)/forgot-password/page.tsx
+++ src/app/(pages)/forgot-password/page.tsx
@@ -5,9 +5,17 @@ import { FormPage } from '@/core/FormPage';
 import { Header } from './Header';
 import { EmailFormGroup } from '@/view/components/EmailFormGroup';
 import useForgotPasswordController from '@/view/controllers/useForgotPasswordController';
+import { SuccessState } from '@/core/State';
+import { Feedback } from './Feedback';
 
 export default function ForgotPassword() {
   const { formController } = useForgotPasswordController();
+
+  if (formController.state instanceof SuccessState) {
+    return (
+      <Feedback />
+    );
+  }
 
   return (
     <FormPage
